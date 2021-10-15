3a628be33d89d762444803ea3ebbdf3c8d7cbceb
wanhose <wanhose.development@gmail.com>
2021-10-15 15:14:07 +0200

    feat(routes): add acronym get route

--- src/routes/v1/acronym.ts
+++ src/routes/v1/acronym.ts
@@ -1,4 +1,11 @@
 import { FastifyInstance, RouteShorthandOptions } from "fastify";
+import prisma from "prisma/client";
+
+type GetAcronymQuerystring = {
+  from: number;
+  limit: number;
+  search: string;
+};
 
 /* eslint-disable @typescript-eslint/no-empty-function */
 
@@ -9,7 +16,59 @@ export default (
 ) => {
   server.delete("/acronym/:acronym", (request, reply) => {});
 
-  server.get("/acronym", (request, reply) => {});
+  server.get<{ Querystring: GetAcronymQuerystring }>(
+    "/acronym",
+    {
+      schema: {
+        querystring: {
+          type: "object",
+          properties: {
+            from: {
+              type: "integer",
+            },
+            limit: {
+              type: "integer",
+            },
+            search: {
+              type: "string",
+            },
+          },
+          required: ["from", "limit", "search"],
+        },
+      },
+    },
+    async (request, reply) => {
+      const { from, limit, search } = request.query;
+      const results = await prisma.acronym.findMany({
+        select: {
+          acronym: true,
+          id: true,
+          meaning: true,
+        },
+        skip: from,
+        take: limit,
+        where: {
+          OR: [
+            {
+              acronym: {
+                contains: search,
+                mode: "insensitive",
+              },
+            },
+            {
+              meaning: {
+                contains: search,
+                mode: "insensitive",
+              },
+            },
+          ],
+          deletedAt: null,
+        },
+      });
+
+      reply.send({ results });
+    }
+  );
 
   server.post("/acronym", (request, reply) => {});
 
